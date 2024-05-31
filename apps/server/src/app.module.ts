5bb374f8e716c4da0cef47f607be943f60415679
Pedro Teixeira <pedro.teixeira@diconium.com>
2024-05-31 09:28:30 +0100

    setup mongodb and openapi

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://mongoadmin:admin@localhost:27017'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
