d2fdb82b6ba157f290a9f12c3370141a56305360
Philipe Almeida <117909435+almeiphi@users.noreply.github.com>
2024-07-10 16:49:20 +0100

    feature: 42 deployment to azure (#62)
    * feat: github workflow to deploy docker image to azure
    
    * chore: update location to use west europe
    
    * chore: update dockerfile path
    
    * chore: adding name property
    
    * chore: including log on bootstrap for testing purpose
    
    * feat: setting env variables in azure container instance
    
    * feat: remove duplicated database connection instance and adding database uri validation
    
    * chore: trigger the container instance
    
    * chore: udpate deploy to azure yaml with some new steps for delete existing aci and debug statements
    
    * chore: updating for re-trigger the docker image deploy
    
    * feat: update the default value of port to 8080
    
    * feat: update the env var port name
    
    * feat: updating the name of container instances for not create a new one in every deploy

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   app.useGlobalFilters(new MongooseValidationFilter());
 
   const port = configService.get<number>('port') ?? 8080;
-  console.log(`Application is starting on port ${port}`); // Log for testing purposes, changing just for deploy trigger
+  console.log(`Application is starting on port: ${port}`); // Log for testing purposes, changing just for deploy trigger
   await app.listen(port);
 }
 bootstrap();
