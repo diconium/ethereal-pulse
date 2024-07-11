55fd34d1cfb4d6f2d857eab2f9c76666c262a586
Pedro Teixeira <pedro.teixeira@diconium.com>
2024-07-11 17:09:40 +0100

    envs

--- apps/server/src/config/configuration.ts
+++ apps/server/src/config/configuration.ts
@@ -1,23 +1,27 @@
-export default () => ({
-  port: parseInt(process.env.MS_PORT ?? '8080', 10),
-  database: {
-    uri: process.env.DATABASE_URI ?? '',
-  },
-  ethereal: {
-    username: process.env.ETHEREAL_USERNAME,
-    password: process.env.ETHEREAL_PASSWORD,
-  },
-  providers: {
-    aws: {
-      region: process.env.AWS_CFG_REGION,
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+export default () => {
+  console.log("🚀 ~ process.env.DATABASE_URI:", process.env.DATABASE_URI)
+  
+  return {
+    port: parseInt(process.env.MS_PORT ?? '8080', 10),
+    database: {
+      uri: process.env.DATABASE_URI ?? '',
     },
-    azure: {
-      connectionString: process.env.AZURE_CONNECTION_STRING,
+    ethereal: {
+      username: process.env.ETHEREAL_USERNAME,
+      password: process.env.ETHEREAL_PASSWORD,
     },
-    common: {
-      cloudProviderName: process.env.CLOUD_PROVIDER_NAME,
+    providers: {
+      aws: {
+        region: process.env.AWS_CFG_REGION,
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      },
+      azure: {
+        connectionString: process.env.AZURE_CONNECTION_STRING,
+      },
+      common: {
+        cloudProviderName: process.env.CLOUD_PROVIDER_NAME,
+      },
     },
-  },
-});
+  }
+};
