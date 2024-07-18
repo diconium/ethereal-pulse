d448ff44e6fd2187e396181cb60838a3b1a1be35
danielfernandes2022 <daniel.fernandes@diconium.com>
2024-07-18 15:36:47 +0100

    Improve code from PR suggestions

--- sdks/typescript/src/index.ts
+++ sdks/typescript/src/index.ts
@@ -1,8 +1,5 @@
-import { EtherealPulse } from './EtherealPulse';
-
 export {
   IEtherealPulse,
   ISendEmailRequest,
 } from './interfaces/email-services.interface';
-export { DEFAULT_ETH_PULSE_ENDPOINT } from './constants/common.constants';
-export { EtherealPulse };
+export { EtherealPulse } from './EtherealPulse';
