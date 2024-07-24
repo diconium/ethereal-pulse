6b624c8bb714331a4100c03c9ec9e7ecde5838f1
Pedro Teixeira <pedro.teixeira@diconium.com>
2024-07-24 10:13:38 +0100

    skip broken test

--- sdks/typescript/jest.config.ts
+++ sdks/typescript/jest.config.ts
@@ -6,22 +6,24 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
-  // The directory where Jest should output its coverage files
-  coverageDirectory: 'coverage',
-  // Indicates which provider should be used to instrument code for coverage
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: './coverage',
   coverageProvider: 'v8',
-  // A preset that is used as a base for Jest's configuration
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
   preset: 'ts-jest',
-  // Sets the path to the prettier node module used to update inline snapshots.
   prettierPath: null,
-  // Use this configuration option to add custom reporters to Jest
   reporters: process.env.GITHUB_ACTIONS
     ? [['github-actions', { silent: false }], 'summary']
     : ['default'],
-  // A list of paths to directories that Jest should use to search for files in
-  roots: ['<rootDir>/src/', '<rootDir>/tests/'],
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  testPathIgnorePatterns: ['/node_modules/', '/lib/'],
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
 };
 
 export default config;
