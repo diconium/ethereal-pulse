c735ed943c75fb0f4dde61c0a537586dd6b173ee
Philipe Almeida <philipe.almeida@diconium.com>
2024-08-02 14:31:58 +0100

    feat: update to web app map env variables in github workflow

--- apps/client/app/services/auth/strategies/github.server.ts
+++ apps/client/app/services/auth/strategies/github.server.ts
@@ -2,24 +2,17 @@ import { GitHubStrategy } from 'remix-auth-github';
 import { sessionStorage } from '~/services/session.server';
 
 function initializeGitHubStrategy() {
-  const {
-    WEBAPP_GITHUB_CLIENT_ID,
-    WEBAPP_GITHUB_CLIENT_SECRET,
-    WEBAPP_GITHUB_CALLBACK_URL,
-  } = process.env;
-  if (
-    !WEBAPP_GITHUB_CLIENT_ID ||
-    !WEBAPP_GITHUB_CLIENT_SECRET ||
-    !WEBAPP_GITHUB_CALLBACK_URL
-  ) {
+  const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, GITHUB_CALLBACK_URL } =
+    process.env;
+  if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET || !GITHUB_CALLBACK_URL) {
     throw new Error('Missing required GitHub environment variables');
   }
 
   return new GitHubStrategy(
     {
-      clientID: WEBAPP_GITHUB_CLIENT_ID,
-      clientSecret: WEBAPP_GITHUB_CLIENT_SECRET,
-      callbackURL: WEBAPP_GITHUB_CALLBACK_URL,
+      clientID: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+      callbackURL: GITHUB_CALLBACK_URL,
     },
     async ({ profile }) => {
       const userId = profile.id;
