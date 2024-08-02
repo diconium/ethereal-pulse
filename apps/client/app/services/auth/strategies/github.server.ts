2a8de962a5a67d0aa60fee09da9a9c62d971eb36
Philipe Almeida <philipe.almeida@diconium.com>
2024-08-02 13:22:42 +0100

    chore: update github env vars name

--- apps/client/app/services/auth/strategies/github.server.ts
+++ apps/client/app/services/auth/strategies/github.server.ts
@@ -2,17 +2,24 @@ import { GitHubStrategy } from 'remix-auth-github';
 import { sessionStorage } from '~/services/session.server';
 
 function initializeGitHubStrategy() {
-  const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, GITHUB_CALLBACK_URL } =
-    process.env;
-  if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET || !GITHUB_CALLBACK_URL) {
+  const {
+    WEBAPP_GITHUB_CLIENT_ID,
+    WEBAPP_GITHUB_CLIENT_SECRET,
+    WEBAPP_GITHUB_CALLBACK_URL,
+  } = process.env;
+  if (
+    !WEBAPP_GITHUB_CLIENT_ID ||
+    !WEBAPP_GITHUB_CLIENT_SECRET ||
+    !WEBAPP_GITHUB_CALLBACK_URL
+  ) {
     throw new Error('Missing required GitHub environment variables');
   }
 
   return new GitHubStrategy(
     {
-      clientID: GITHUB_CLIENT_ID,
-      clientSecret: GITHUB_CLIENT_SECRET,
-      callbackURL: GITHUB_CALLBACK_URL,
+      clientID: WEBAPP_GITHUB_CLIENT_ID,
+      clientSecret: WEBAPP_GITHUB_CLIENT_SECRET,
+      callbackURL: WEBAPP_GITHUB_CALLBACK_URL,
     },
     async ({ profile }) => {
       const userId = profile.id;
