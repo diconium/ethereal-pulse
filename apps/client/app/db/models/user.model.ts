43057f8eea10d5dff1124574cf9724823ca7e8a7
Philipe Almeida <117909435+almeiphi@users.noreply.github.com>
2024-09-13 15:35:33 +0100

    fix: udpate import to userSchema (#115)
    * fix: udpate import to userSchema
    
    * fix: udpate unit test failed

--- apps/client/app/db/models/user.model.ts
+++ apps/client/app/db/models/user.model.ts
@@ -1,5 +1,5 @@
 import { mongoose } from '../index';
-import { UserSchema } from 'apps/server/src/database/schemas/user.schema';
+import { UserSchema } from '../schemas/user.schema';
 
 const UserModel = mongoose.models.User || mongoose.model('User', UserSchema);
 
