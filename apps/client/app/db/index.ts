c0436f998b965c2588d3fa0a224b7240a0a0e9c0
Philipe Almeida <117909435+almeiphi@users.noreply.github.com>
2024-09-14 23:21:25 +0100

    fix: 110 sign up sign in bff deploy error (#117)
    * fix: udpate import to userSchema
    
    * fix: udpate unit test failed
    
    * chore: adding the webapp public url env variable to web app
    
    * fix: increae timeout mongodb connection and add error handler for db connection

--- apps/client/app/db/index.ts
+++ apps/client/app/db/index.ts
@@ -6,6 +6,21 @@ if (!uri) {
   throw new Error('DATABASE_WEBAPP_URI is not defined');
 }
 
-mongoose.connect(uri);
+mongoose.connect(uri, {
+  serverSelectionTimeoutMS: 30000,
+  socketTimeoutMS: 45000,
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to DB');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('Mongoose connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected from DB');
+});
 
 export { mongoose };
