81e245b0b2f8e53b4a3931811d7a421a9f54b539
Philipe Almeida <philipe.almeida@diconium.com>
2024-09-16 11:25:30 +0100

    fix: rename env var related to database and public web app uri

--- apps/client/app/constants/api.constants.ts
+++ apps/client/app/constants/api.constants.ts
@@ -1,7 +1,7 @@
 export const API_BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:5173'
-    : process.env.WEBAPP_PUBLIC_URI;
+    : process.env.WEBAPP_URI;
 
 export const API_ROUTES = {
   USERS: '/users',
